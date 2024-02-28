4e624fb604e9199f9dd762573eff64e7df74c43a
PBeGood4 <pedrocbeirao@gmail.com>
2024-02-28 10:04:48 +0000

    handle basic collisions

--- draw.py
+++ draw.py
@@ -1,9 +1,22 @@
 import pygame
 
 def draw_player(gamestate, surface):
-    rect = pygame.Rect(gamestate.player_pos.x*50, gamestate.player_pos.y*50, 50,50)
-    pygame.draw.rect(surface, (200,200,20), rect)
+    pygame.draw.circle(surface, (200,200,20), (gamestate.player.x*50 + 25, gamestate.player.y*50 + 25), 20)
+
+    pygame.draw.rect(surface, (0,0,0), (gamestate.player.x*50, gamestate.player.y*50 - 50, 50,50), 1)
+    pygame.draw.rect(surface, (0,0,0), (gamestate.player.x*50, gamestate.player.y*50 + 50, 50,50), 1)
+    pygame.draw.rect(surface, (0,0,0), (gamestate.player.x*50 - 50, gamestate.player.y*50, 50,50), 1)
+    pygame.draw.rect(surface, (0,0,0), (gamestate.player.x*50 + 50, gamestate.player.y*50, 50,50), 1)
+
+def draw_walls(gamestate, surface):
+    for wall in gamestate.level.walls:
+        pygame.draw.rect(surface, (20,20,200), (wall.x*50, wall.y*50, 50,50))
+
+def draw_molecules(gamestate, surface):
+    for molecule in gamestate.level.molecules:
+        pygame.draw.circle(surface, (200,20,20), (molecule.x*50 + 25, molecule.y*50 + 25), 20)
 
 def draw_screen(gamestate, surface):
-    #draw_board()
+    draw_walls(gamestate, surface)
+    draw_molecules(gamestate, surface)
     draw_player(gamestate, surface)
--- game.py
+++ game.py
@@ -8,19 +8,64 @@ class Position:
         self.x = x
         self.y = y
 
+    def __eq__(self, other): 
+        if not isinstance(other, Position):
+            return NotImplemented
+
+        return (self.x == other.x) and (self.y == other.y)
+
 class GameState:
     def __init__(self):
         self.level = Level()
-        self.player_pos = self.level.player_start_pos
+        self.player = self.level.player_start
+        self.grabbed_molecules = []
 
     def reset(self):
         self.__init__()
     
     def try_to_move(self, direction):
-        self.player_pos.x += direction[0]
-        self.player_pos.y += direction[1]
+        new_player = Position(self.player.x + direction[0], self.player.y + direction[1])
+
+        for wall in self.level.walls:
+            if wall == new_player:
+                return
+            
+        self.player = new_player
+
+
 
 class Level:
+    def load_level_from_string(self, width, height, string):
+        if len(string) < (width*height):
+            print ("Level size is incorrect")
+            # handle error
+
+        for y in range(0, height):
+            for x in range(0, width):
+                index = width * y + x
+
+                if string[index] == 'X':
+                     self.walls.append(Position(x, y))
+                elif string[index] == 'P':
+                     self.player_start = Position(x, y)
+                elif string[index] == 'M':
+                     self.molecules.append(Position(x, y))
+                    
     def __init__(self):
-        self.player_start_pos = Position(6,6)
-        self.walls = [Position(0,0),Position(0,1)]
+        self.player_start = Position(0,0)
+        self.walls = []
+        self.molecules = []
+
+        self.load_level_from_string(13, 13,  "XXXXXXXXXXXXX"
+                                             "XXXXXX XXXXXX"
+                                             "XXXXX P XXXXX"
+                                             "XXXX     XXXX"
+                                             "XXX       XXX"
+                                             "XX  M   M  XX"
+                                             "X           X"
+                                             "XX         XX"
+                                             "XXX M   M XXX"
+                                             "XXXX     XXXX"
+                                             "XXXXX   XXXXX"
+                                             "XXXXXX XXXXXX"
+                                             "XXXXXXXXXXXXX")
