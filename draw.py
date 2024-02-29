9c0c9750b4dfb67049ece047ccc23b7e3f0d732a
PBeGood4 <pedrocbeirao@gmail.com>
2024-02-29 12:03:48 +0000

    New collision code

--- draw.py
+++ draw.py
@@ -9,12 +9,16 @@ def draw_player(gamestate, surface):
     pygame.draw.rect(surface, (0,0,0), (gamestate.player.position.x*50 + 50, gamestate.player.position.y*50, 50,50), 1)
 
 def draw_walls(gamestate, surface):
-    for wall in gamestate.level.walls:
-        pygame.draw.rect(surface, (20,20,200), (wall.x*50, wall.y*50, 50,50))
+    for y in range(0, gamestate.level.height):
+        for x in range(0, gamestate.level.width):
+            if gamestate.level.walls[y][x] != 0:
+                pygame.draw.rect(surface, (20,20,200), (x*50, y*50, 50,50))
 
 def draw_molecules(gamestate, surface):
-    for molecule in gamestate.level.molecules:
-        pygame.draw.circle(surface, (200,20,20), (molecule.position.x*50 + 25, molecule.position.y*50 + 25), 20)
+    for y in range(0, gamestate.level.height):
+        for x in range(0, gamestate.level.width):
+            if gamestate.level.molecules[y][x] != None:
+                pygame.draw.circle(surface, (200,20,20), (x*50 + 25, y*50 + 25), 20)
 
 def draw_screen(gamestate, surface):
     draw_walls(gamestate, surface)
--- game.py
+++ game.py
@@ -38,45 +38,54 @@ class GameState:
 
         self.grabbed_molecules = new_grabbed_molecules
     
+    def try_to_move_molecule(self, molecule, direction):
+        print (0)
+        if (self.level.walls[molecule.position.y + direction[1]][molecule.position.x + direction[0]] == 0):
+            print (1)
+            if (self.level.molecules[molecule.position.y + direction[1]][molecule.position.x + direction[0]] == None):
+                molecule.position.x += direction[0]
+                molecule.position.y += direction[1]
+                print (2)
+    
     def try_to_move(self, direction):
-        collision_pos = Position(self.player.position.x, self.player.position.y)
-        collision_index = -1 # -1=looking 0=found_empty 1=found_wall
+        if self.try_to_move_molecule(self.player, direction):
+            pass
+            # self.try_to_grab()
 
-        while collision_index == -1:
-            collision_index = 0
 
-            collision_pos.x += direction[0]
-            collision_pos.y += direction[1]
+        # collision_pos = Position(self.player.position.x, self.player.position.y)
+        # collision_index = -1 # -1=looking 0=found_empty 1=found_wall
 
-            for molecule in self.level.molecules:
-                if molecule == collision_pos:
-                    collision_index = -1
-                    break
+        # while collision_index == -1:
+        #     collision_index = 0
 
-            for wall in self.level.walls:
-                if wall == collision_pos:
-                    collision_index = 1
-                    break
+        #     collision_pos.x += direction[0]
+        #     collision_pos.y += direction[1]
+
+        #     for molecule in self.level.molecules:
+        #         if molecule == collision_pos:
+        #             collision_index = -1
+        #             break
+
+        #     for wall in self.level.walls:
+        #         if wall == collision_pos:
+        #             collision_index = 1
+        #             break
             
-        if collision_index == 0:
-            self.player.position.x += direction[0]
-            self.player.position.y += direction[1]
+        # if collision_index == 0:
+        #     self.player.position.x += direction[0]
+        #     self.player.position.y += direction[1]
 
-            for grabbed_molecule_index in self.grabbed_molecules:
-                self.level.molecules[grabbed_molecule_index].x += direction[0]
-                self.level.molecules[grabbed_molecule_index].y += direction[1]
+        #     for grabbed_molecule_index in self.grabbed_molecules:
+        #         self.level.molecules[grabbed_molecule_index].x += direction[0]
+        #         self.level.molecules[grabbed_molecule_index].y += direction[1]
 
-            self.try_to_grab()
+        #     self.try_to_grab()
 
 
 class Molecule():
     def __init__(self, char, x, y):
         self.position = Position(x,y)
-
-        self.is_player = False
-        if char.isupper():
-            self.is_player = True
-            char = char.lower()
         
         self.max_grab = 0
         if char == 'h':
@@ -95,25 +104,35 @@ class Level:
         if len(string) < (width*height):
             print ("Level size is incorrect")
             # handle error
+        
+        self.width = width
+        self.height = height
+
+        self.walls = [[0] * width for i in range(0, height)]
+        self.molecules = [[None] * width for i in range(0, height)]
 
         for y in range(0, height):
             for x in range(0, width):
                 index = width * y + x
 
                 if string[index] == 'X':
-                    self.walls.append(Position(x,y))
+                    self.walls[y][x] = 1
                 elif string[index] != ' ':
                     molecule = Molecule(string[index], x, y)
-                    self.molecules.append(molecule)
-
                     if string[index].isupper():
                         self.player_start = molecule
+                    else:
+                        self.molecules[y][x] = molecule
+
                     
     def __init__(self):
         self.player_start = Molecule('h', 0, 0)
 
         self.walls = []
         self.molecules = []
+
+        self.width = 0
+        self.height = 0
 
         self.load_level_from_string(13, 13,  "XXXXXXXXXXXXX"
                                              "XXXXXX XXXXXX"
